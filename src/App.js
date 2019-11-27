d76988334f32ce91082528b23f7529d364745a53
nitinsoni967 <34713241+nitinsoni967@users.noreply.github.com>
2019-11-27 14:37:30 +0530

    Logo changed to loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import loadingGif from './loader1.gif'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={loadingGif} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
